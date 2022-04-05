2ac57829f6a1b26bd542d1aa42f6931dba80d0e4
Gustaf Järgren <gurreja@gmail.com>
2022-04-05 20:34:48 +0200

    fix: Change to not use internal jei, reloading datapacks for jei don't work anymore. (#5)

--- src/main/java/se/gory_moon/idp/client/jei/JEIPlugin.java
+++ src/main/java/se/gory_moon/idp/client/jei/JEIPlugin.java
@@ -1,27 +1,17 @@
 package se.gory_moon.idp.client.jei;
 
-import com.google.common.collect.ImmutableList;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.constants.VanillaRecipeCategoryUid;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.recipe.IRecipeManager;
-import mezz.jei.api.registration.IAdvancedRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
-import mezz.jei.api.runtime.IIngredientManager;
-import mezz.jei.api.runtime.IJeiRuntime;
-import mezz.jei.plugins.jei.info.IngredientInfoRecipe;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import se.gory_moon.idp.InfoDataPack;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -30,15 +20,9 @@ public class JEIPlugin implements IModPlugin {
     private static final Logger LOGGER = LogManager.getLogger(JEIPlugin.class);
 
     @Nullable
-    private static InfoRecipeManager recipeManager;
-    @Nullable
-    private List<IngredientInfoRecipe> dummyRecipes;
-    @Nullable
-    public static List<IngredientInfoRecipe> previousRecipes;
-    @Nullable
     public static Map<List<ItemStack>, List<Component>> preInfoData;
     @Nullable
-    private static IIngredientManager ingredientManager;
+    private static IRecipeRegistration recipeRegistration;
 
     @Override
     public ResourceLocation getPluginUid() {
@@ -47,56 +31,27 @@ public class JEIPlugin implements IModPlugin {
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        ingredientManager = registration.getIngredientManager();
+        recipeRegistration = registration;
         tryToSetData();
-        dummyRecipes = IngredientInfoRecipe.create(ingredientManager, Collections.singletonList(new ItemStack(Items.BARRIER)), VanillaTypes.ITEM, new TextComponent("Dummy text, plz ignore ;)"));
-    }
-
-    @Override
-    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
-        IRecipeManager recipeManager = jeiRuntime.getRecipeManager();
-        if (dummyRecipes != null)
-            dummyRecipes.forEach(recipe -> recipeManager.hideRecipe(recipe, VanillaRecipeCategoryUid.INFORMATION));
-    }
-
-    @Override
-    public void registerAdvanced(IAdvancedRegistration registration) {
-        recipeManager = new InfoRecipeManager(registration.getJeiHelpers().getStackHelper());
-        tryToSetData();
-        registration.addRecipeManagerPlugin(recipeManager);
     }
 
     private void tryToSetData() {
-        if (recipeManager != null && ingredientManager != null)
-            recipeManager.setIngredientManager(ingredientManager);
-
-        // If we have gotten the data before creating the manager use this
-        if (previousRecipes != null && recipeManager != null)
-            recipeManager.setRecipes(previousRecipes);
-
-        if (preInfoData != null && ingredientManager != null) {
+        if (preInfoData != null) {
             addRecipes(preInfoData);
             preInfoData = null;
         }
     }
 
     public static void addInfoRecipes(Map<List<ItemStack>, List<Component>> infoData) {
-        if (ingredientManager == null) {
-            preInfoData = infoData;
-            return;
-        }
-        addRecipes(infoData);
+        preInfoData = infoData;
     }
 
-    @SuppressWarnings("ConstantConditions")
     private static void addRecipes(Map<List<ItemStack>, List<Component>> infoData) {
         LOGGER.debug("Adding ingredient info");
-        ImmutableList.Builder<IngredientInfoRecipe> builder = ImmutableList.builder();
+
         infoData.forEach((key, value) -> {
-            builder.addAll(IngredientInfoRecipe.create(ingredientManager, key, VanillaTypes.ITEM, value.toArray(Component[]::new)));
+            if (recipeRegistration != null)
+                recipeRegistration.addIngredientInfo(key, VanillaTypes.ITEM, value.toArray(Component[]::new));
         });
-        previousRecipes = builder.build();
-        if (recipeManager != null)
-            recipeManager.setRecipes(previousRecipes);
     }
 }
